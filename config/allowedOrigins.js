91d32a315448c60a1d89e7ca3b4f86d7a61d8b0b
Dauvnir <134309790+Dauvnir@users.noreply.github.com>
2024-07-20 18:24:27 +0200

    database uri CORS WHITELIST added.

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,8 @@
-const whitelist = ["https://project-astral.onrender.com"];
+require("dotenv").config;
+
+const whitelist = [
+	"https://project-astral.onrender.com",
+	process.env.DATABASE_URI,
+];
 
 module.exports = whitelist;
