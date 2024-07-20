6d75bea632d781fca147b1d53cbaa589ebfcaf8d
Dauvnir <134309790+Dauvnir@users.noreply.github.com>
2024-07-20 18:10:31 +0200

    database uri connect added.

--- db.js
+++ db.js
@@ -2,10 +2,17 @@ const Pool = require("pg").Pool;
 require("dotenv").config;
 
 const pool = new Pool({
-	user: process.env.USER,
-	password: process.env.PGPASSWORD,
-	host: process.env.DBHOST,
-	port: 5432,
-	database: "astraldb",
+	connectionString: process.env.DATABASE_URI,
+	ssl: {
+		rejectUnauthorized: false, // This is required for some hosted services like Heroku
+	},
 });
+
+// const pool = new Pool({
+// 	user: process.env.USER,
+// 	password: process.env.PGPASSWORD,
+// 	host: process.env.DBHOST,
+// 	port: 5432,
+// 	database: "astraldb",
+// });
 module.exports = pool;
